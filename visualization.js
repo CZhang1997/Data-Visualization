5d99a280f71e49262a19086aad521182ba0094b9
Churong Zhang <cxz173430@utdallas.edu>
2020-03-24 18:20:23 -0500

    linked files

---
+++ visualization.js
@@ -0,0 +1,12 @@
+var dataArray = [5,11,18];
+
+var svg = d3.select("body").append("svg")
+            .attr("height","100%")
+            .attr("width", "100%");
+
+
+            var newX = 300;
+            svg.append("circle")
+                            .attr("cx","350")
+                            .attr("cy","350")
+                            .attr("r","300");
