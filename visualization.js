5380382b69318318fc6605ec10ab25c5d0e166c3
Churong Zhang <cxz173430@utdallas.edu>
2020-03-25 12:35:49 -0500

    template

--- visualization.js
+++ visualization.js
@@ -1,12 +1,21 @@
 var dataArray = [5,11,18];
+var margin = {left:50, right: 50, top:40, bottom:0, center: "50%"};
+
 
 var svg = d3.select("body").append("svg")
             .attr("height","100%")
             .attr("width", "100%");
 
+var template = svg.append("g");//.attr("transform","translate("+margin.left+","+margin.top+")");
 
-            var newX = 300;
-            svg.append("circle")
-                            .attr("cx","50%")
-                            .attr("cy","50%")
-                            .attr("r","380");
+    template.append("circle")
+        .attr("cx",margin.center)
+        .attr("cy",margin.center)
+        .attr("fill", "gray")
+        .attr("r","300");
+
+        template.append("circle")
+            .attr("cx",margin.center)
+            .attr("cy",margin.center)
+            .attr("fill", "white")
+            .attr("r","250");
