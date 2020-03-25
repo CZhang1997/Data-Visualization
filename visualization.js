674dc1903fd590756b87c4f86636855d1044432b
Churong Zhang <cxz173430@utdallas.edu>
2020-03-25 13:44:31 -0500

    change with screen size

--- visualization.js
+++ visualization.js
@@ -3,7 +3,7 @@ var margin = {left:0, right: 50, top:0, bottom:0, center: "50%"};
 
 var mid_width = screen.width / 2;
 var mid_height = screen.height /2.4;
-
+var radius = 250;
 var svg = d3.select("body").append("svg")
             .attr("height","100%")
             .attr("width", "100%");
@@ -14,25 +14,25 @@ var template = svg.append("g").attr("transform","translate("+margin.left+","+mar
         .attr("cx",mid_width)
         .attr("cy",mid_height)
         .attr("fill", "#cdc1c5")
-        .attr("r","300");
+        .attr("r",radius + 50);
 
     template.append("circle")
             .attr("cx",mid_width)
             .attr("cy",mid_height)
             .attr("fill", "white")
-            .attr("r","250");
+            .attr("r", radius);
 
     template.append("line")
       .attr("stroke-width", "2")
       .attr("stroke", "red")
-      .attr("x1", mid_width* 0.66)
+      .attr("x1", mid_width - radius)
       .attr("y1", mid_height)
-      .attr("x2", mid_width * 1.33)
+      .attr("x2", mid_width + radius)
       .attr("y2", mid_height);
     template.append("line")
         .attr("stroke-width", "2")
         .attr("stroke", "red")
         .attr("x1", mid_width)
-        .attr("y1", mid_height* 0.35)
+        .attr("y1", mid_height - radius)
         .attr("x2", mid_width)
-        .attr("y2", mid_height * 1.65);
+        .attr("y2", mid_height + radius);
